035323357a54384d0e2206e221f813251cfaec5f
ljubicamiljkovic56 <ljubica.m19@gmail.com>
2025-07-16 21:46:00 +0200

    Modified User class

--- reddit/src/main/java/com/example/model/User.java
+++ reddit/src/main/java/com/example/model/User.java
@@ -1,5 +1,19 @@
 package com.example.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
 public class User {
 
 }
